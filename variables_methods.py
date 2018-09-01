f15fceebf026894f69968d6cfaa57c94fb87b942
Unknown <brian@brianweis.net>
2018-08-31 18:03:29 -0600

    Test Upload

---
+++ variables_methods.py
@@ -0,0 +1,17 @@
+a = 5
+b = 10
+my_variable= 56
+
+string_variable = "hello"
+
+single_qoutes = 'strings can have single quotes'
+
+#print(my_variable)
+
+##
+
+def my_print_method():
+    print("Hello")
+    print("World")
+
+my_print_method();
