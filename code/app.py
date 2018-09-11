f2b4129ca7cfcc3fa4538b16c2aef45ea6807b79
DESKTOP-QF1EDR5\brian <brian@brianweis.net>
2018-09-10 18:25:35 -0600

    More error checking

--- code/app.py
+++ code/app.py
@@ -1,13 +1,30 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
 api = Api(app)
 
-class Student(Resource):
+items = []
+
+class Item(Resource):
     def get(self,name):
-        return {'student':name}
+        item = next(filter(lambda x: x['name'] ==name, items), None)
+        return {'item':item}, 200 if item else 404
+        
 
-api.add_resource(Student, '/student/<string:name>')
+    def post(self,name):
+        if next(filter(lambda x: x['name'] ==name, items), None) is not None:
+            return {'message': "An item with name {} already exists.".format(name)}, 400
+        data  = request.get_json()
+        item = {'name':name,'price':data['price']}
+        items.append(item)
+        return item, 201
 
-app.run(port=5000)
+class ItemList(Resource):
+    def get(self):
+        return {'items':items}        
+
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
+
+app.run(port=5000, debug=True)
