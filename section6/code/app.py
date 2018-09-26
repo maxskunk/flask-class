3bd859a17eed0d09255f540ba23b04b5ae59f76c
DESKTOP-QF1EDR5\brian <brian@brianweis.net>
2018-09-26 16:41:26 -0600

    Almost Done converting to SQL Alchamy

--- section6/code/app.py
+++ section6/code/app.py
@@ -25,4 +25,4 @@ if __name__ == '__main__':  # This prevents issues if this library is imported,
     # makes sure this only runs from the main app
     from db import db
     db.init_app(app)
-    app.run(port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
--- section6/code/resources/user.py
+++ section6/code/resources/user.py
@@ -1,4 +1,3 @@
-import sqlite3
 from flask_restful import Resource, reqparse
 from models.user import UserModel
 
@@ -21,13 +20,8 @@ class UserRegister(Resource):
         data = UserRegister.parser.parse_args()
         if UserModel.find_by_username(data['username']):
             return {"message": "A user with that username already exists"}, 400
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
 
-        query = "INSERT INTO users VALUES (NULL,?,?)"
-        cursor.execute(query, (data['username'], data['password'],))
-
-        connection.commit()
-        connection.close()
+        user = UserModel(data['username'], data['password'])
+        user.save_to_db()
 
         return {"messsage": "User created successfully."}, 201
